e865115accce6071b0213d4e68e0291c74bedb56
gitprimos <primos@126.com>
2015-04-08 19:44:28 +0800

    add underscore

---
+++ underscore/demo.js
@@ -0,0 +1,25 @@
+function integerArray() {
+    var list = [1, 2, 3, 4];
+    console.log(list);
+    var des = _.without(list, 1);
+    console.log(des)
+}
+
+function objectArray() {
+    var list = [{
+        id: 1,
+        value: 1
+    }, {
+        id: 2,
+        value: 2
+    }];
+    console.log(list);
+    var des = _.without(list, {
+        id: 1,
+        value: 1
+    });
+    console.log(des)
+}
+
+integerArray();
+objectArray();
